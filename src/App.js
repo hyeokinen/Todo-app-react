400086c2557a1af33be120a8ceabb490cc44e2fc
hyeokinen <kkunh97@naver.com>
2023-01-25 10:26:06 +0900

    # props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Greeting from './components/Greeting';
-import FoodList from './components/FoodList';
-import Hello from './components/Hello';
+
+import ItemMain from './components/item/ItemMain';
+
 
 function App() {
   return (
     <>
-    {/* <Greeting />
-    <FoodList/> */}
-    <Hello/>
-   
-   
-   </>
-
+      <ItemMain />
+    </>
   );
 }
 
